4ab36528c641b5f49fc8e5a8d8db0f8c1dde065e
pyokun <542638591@qq.com>
2021-05-31 12:30:49 +0900

    Update InterpolationGAN.py

--- digits_classification/scripts/dlow_model/InterpolationGAN.py
+++ digits_classification/scripts/dlow_model/InterpolationGAN.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-from model import BaseNetwork as Base
+from dlow_model import BaseNetwork as Base
 from util import utils
 
 
